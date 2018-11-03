543553ef0e187f815a409895ff947d9ada149cc3
dyingapple <dyingapple@gmail.com>
2018-11-03 17:55:48 +0800

    update gate.py

--- gate/gate.py
+++ gate/gate.py
@@ -46,11 +46,15 @@ def upload_record(uid, files, createTime, modelName):
 	global save_location
 	record_file = save_location + "/record.json"
 	record = {}
-
-
+	'''
+	 Save metadata into two parts for client to dig
+	  1. Task ID
+	  2. File ID
+	'''
 	with open(record_file , 'w+') as f:
 		record['task'] = str(uid)
 		record['time'] = str(createTime)
+		# If > one file, create id for each file_id, if only one -> file_id = task_id
 		if len(files.getlist("file")) == 1:
 			record['file_id'] = str(uid)
 			record['file_name'] = files.getlist("file")[0].filename
@@ -75,13 +79,12 @@ def save_file(files):
 		return 0
 	os.makedirs(directory)
 
-	# Store every files from upload to UUID/sample.file
+	# Store every files from upload to UUID/filename
 	for l in tmp:
 		filename = secure_filename(l.filename)
 		l.save(os.path.join(directory, l.filename))
-	# Return a UUID as the taskID
-	return spid
-
+		# Return a UUID as the taskID
+	return spid  
 
 
 
@@ -99,22 +102,27 @@ def v1_predict(modelName, action):
 
 	if action == 'predict' and request.method == 'POST':
 		if 'file' not in request.files:
-			return Response("{'error':'no file', 'usage':'Please add key:file in the header'}", status=406, mimetype='application/json')
+			msg = "{'error':'no file', 'usage':'Please add key:file in the header'}"
+			code = 406
+			return Response(msg, status=code, mimetype='application/json')
 		spid = save_file(request.files)
+
 		if spid == 0:
-			return Response("{'error':'uuid repeated or internal error, please try again'}", status=500, mimetype='application/json')
+			msg = "{'error':'uuid repeated or internal error, please try again'}"
+			code = 500
+			return Response(msg, status=code, mimetype='application/json')
 		# Else: no error, then 
 		print(spid)
 
 		createTime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).astimezone(datetime.timezone(datetime.timedelta(hours=8)))
 		content = request.json
-		# record 
 		upload_record(spid, request.files, createTime, modelName)
-
+		# send()
 		return jsonify({ modelName:action, 'UUID':spid})
 
 	elif action == 'result' and request.method == 'GET':
-		if 'file' not in request.files:
+		print(request.form.get('id'))
+		if 'id' not in request.form.get('id'):
 			return Response("{'error':'no id for request', 'usage':'Please add id:id in the header'}", status=406, mimetype='application/json')
 		# Client GET result.
 		pass
